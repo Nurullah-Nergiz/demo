fdc4aefb26d301d5124b375c53c3bef80015569c
Nurullah-Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2020-03-24 21:00:02 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    id="app"
+  >Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quaerat quae nesciunt ipsa ex itaque, ut modi voluptate inventore, assumenda laudantium aliquam tempora ea libero voluptatum hic beatae quisquam corporis!</div>
+</template>
+
+<style lun>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
